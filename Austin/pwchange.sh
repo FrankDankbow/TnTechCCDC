2dfebb5703d289c5a73f41fadfc55ff4ae85a77c
Derek Singh <Derek@Dereks-MacBook-Pro-2.local>
2019-02-22 21:40:38 -0600

    Austin's Files

---
+++ Austin/pwchange.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Edit Passwords"
+#List users with UID over 1000
+
+echo "Enter new password:"
+read pass
+
+cut -d: -f1,3 /etc/passwd | egrep ':[0-9][0-9][0-9][0-9]' | cut -d: -f1 > usersover1000
+echo root >> usersover1000
+for user in `cat usersover1000`
+do
+   echo "$user:$pass" | chpasswd
+        if [ "$?" -eq "0" ]; then
+                echo "$user password change successful: $pass"
+
+        else
+                echo "$user password change failed"
+
+
+        fi
+done
+
+rm usersover1000
